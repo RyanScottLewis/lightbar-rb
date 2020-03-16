ebc8edbff6d2f169f3e6c20147b9a3073d5d4c26
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2020-03-15 23:47:15 -0400

    Open pi blaster device on timer start

--- lib/lightbar/subscriber/light_updater.rb
+++ lib/lightbar/subscriber/light_updater.rb
@@ -6,30 +6,41 @@ module Lightbar
     # Update the light PWM value on value changes via pi-blaster
     class LightUpdater < Base
 
-      def on_init(event)
-        @io = File.open(options.pi_blaster, "w") unless options.dry
-
+      def on_start(event)
         check_pi_blaster
+        open_pi_blaster
       end
 
       def on_change(event)
-        return if options.dry || @io.nil?
-
-        @io.puts("#{options.pin}=#{event.value}")
-        @io.flush
+        update_value(event.value)
       end
 
       def on_stop(event)
-        @io.close unless @io.nil?
+        close_pi_blaster
       end
 
       protected
+
+      def open_pi_blaster
+        @io = File.open(options.pi_blaster, "w") unless options.dry
+      end
 
       def check_pi_blaster
         return if options.dry || File.exists?(options.pi_blaster)
 
         @logger.fatal("pi-blaster path does not exist: #{options.pi_blaster}")
         exit(1)
+      end
+
+      def close_pi_blaster
+        @io.close unless @io.nil?
+      end
+
+      def update_value(value)
+        return if options.dry || @io.nil?
+
+        @io.puts("#{options.pin}=#{value}")
+        @io.flush
       end
 
     end
